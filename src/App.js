ab1a35a3f0a75610fcf7250e4a2ee699b4564fe9
10xc <01053702613a@gmail.com>
2023-04-18 14:51:24 +0900

    환경변수 추가

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import Detail from "./routes/Detail";
 
 
 function App() {
-  return <BrowserRouter>
+  return <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
-      <Route path="/" element={<Home />}/>
+      <Route element={<Home />}/>
       <Route path="/movie/:id" element={<Detail />}/>
     </Routes>
   </BrowserRouter>
